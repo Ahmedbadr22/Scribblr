598c7bf10e4485053b9c44ab4226c82a0bd931b2
Ahmedbadr22 <ahmedbadr200121@gmail.com>
2023-04-18 01:01:15 +0200

    add: req.txt & vercel.json

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p@m1057)un&)#7l-hq#y92j5eci**=48yfnk2i6dl9j*&d_dmp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 # Application definition
 
