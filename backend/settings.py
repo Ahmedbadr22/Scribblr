76473f769df27e5a57d65e3d3304057dc81a6b42
Ahmedbadr22 <ahmedbadr200121@gmail.com>
2023-04-18 01:48:07 +0200

    update db settings

--- backend/settings.py
+++ backend/settings.py
@@ -83,12 +83,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:LfZifv7PnnGReoeVOwIg@containers-us-west-191.railway.app:5985/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'LfZifv7PnnGReoeVOwIg',
+        'HOST': 'containers-us-west-191.railway.app',
+        'PORT': '5985',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
